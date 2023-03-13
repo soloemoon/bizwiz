38e00f608913bd00b8bb5b0c7ab5c89b211df302
Solomon Moon <57546826+soloemoon@users.noreply.github.com>
2023-03-13 01:59:12 -0500

    Update flat_file.py

--- src/bizwiz_sm/flat_file.py
+++ src/bizwiz_sm/flat_file.py
@@ -1,56 +1,37 @@
-class flat_file:
-    
-    def __init__(self, file_path_list):
-        self.file_path_list = file_path_list
+import xlwings as xw
+import os
+import re
+import polars as pl
+import pandas as pd
+import printable
+import duckdb
 
-    def read_multi_excel(self, concat=False):
-       
-        if concat == True:
-            # Read each file into a list
-            read_files = [pl.read_excel(f, ignore_errors = True) for f in self.file_path_list]
-    
-            output = pl.concat(read_files, how='diagonal')
+class flat_file: 
+    def __init__(self):
+        pass
+
+    def read_multi_file(self, file_path_list=None, concat=False, concat_type = 'diagonal'):
+        if concat==True:
+            read_files = [pl.read_excel(f, ignore_errors=True) if os.path.splitext(f)[1] == '.xlsx' else pl.read_csv(f, ignore_errors = True) for f in file_path_list]
+            output = pl.concat(read_files, how=concat_type)
             return output
         
-        elif concat == False:
-            f1, *other = map(pl.read_excel, self.file_path_list)
+        elif concat==False:
+            for f in self.file_path_list:
+                if os.path.splitext(f)[1] == '.xlsx':
+                    f1, *other = map(pl.read_excel, file_path_list)
+                else:
+                    f1, *other = map(pl.read_csv, file_path_list)
             return (f1, *other)
     
-    def read_multi_csv(self, concat=False):
-        if concat == True:
-            # Read each file into a list
-            read_files = [pl.read_csv(f, ignore_errors = True) for f in self.file_path_list]
-    
-            output_fl = pl.concat(read_files, how='diagonal')
-            return output_fl
-        
-        elif concat == False:
-            f1, *other = map(pl.read_csv, self.file_path_list)
-            return (f1, *other)
-        
-    def export_excel_shts(self, workbook, xl_sheet_list, df_names, index = False):
-        app = xw.App(visible = False)
-        wb = xw.Book(workbook)
-        
-        df_dict = dict(zip(xl_sheet_list, df_names))
-        
-        for sht in xl_sheet_list:
-            ws = wb.sheets[sht].clearcontents()
-            ws['A1'].options(pd.Dataframe, header=1, index=index, expand = 'table').value = df_dict[sht]
-            
-        wb.api.RefreshAll()
-        wb.save(workbook)
-        wb.close()
-        app.quit()
-        
     def read_sql_file(self, file_link):
         sql_file = open(file_link, 'r').read().split(';')
         sql_commands = [sc.strip() for sc in sql_file]
-        sql_commands = [re.sub("[^{}]+".format(printable), "", sc) for sc in sql_commands ]
+        sql_commands = [re.sub("[^{}]+".format(printable), "", sc) for sc in sql_commands]
         return sql_commands
-
-    def create_multi_parquet(self, output_directory, df_names):
-        for f in self.file_path_list:
+    
+    def to_multi_parquet(self, output_directory, df_names, file_path_list = None):
+        for f in file_path_list:
             fp = f"{output_directory}{os.path.splitext(f)[0]}.parquet"
             for df in df_names:
                 df.write_parquet(fp, compression='zstd')
@@ -68,9 +49,27 @@ class flat_file:
         start = re.findall(r'select.+?from', sql_query)
         end = re.findall(r'(where.*)', sql_query)
         query = " ".join(start) + f" read_parquet({parquet_path}) " + " ".join(end) 
-        df = duckdb.query(sql_query).to_df()  
+        df = duckdb.query(query).to_df()  
         return df
     
     def dict_from_df(df, key_col, value_col):
         t_dict = df.set_index(key_col).to_dict()[value_col]
         return t_dict
+
+    def export_excel_shts(self, workbook, xl_sheet_list, df_names, index = False):
+        app = xw.App(visible = False)
+        wb = xw.Book(workbook)
+        
+        df_dict = dict(zip(xl_sheet_list, df_names))
+        
+        for sht in xl_sheet_list:
+            ws = wb.sheets[sht].clearcontents()
+            ws['A1'].options(pd.Dataframe, header=1, index=index, expand = 'table').value = df_dict[sht]
+            
+        wb.api.RefreshAll()
+        wb.save(workbook)
+        wb.close()
+        app.quit()
+   
+    
+    
