57120250518ae3b7fd70540bbfa55be9ba43d05b
Solomon Moon <57546826+soloemoon@users.noreply.github.com>
2024-03-10 23:48:09 -0500

    Update __init__.py
    Updated modules

--- bizwiz/__init__.py
+++ bizwiz/__init__.py
@@ -4,5 +4,8 @@ import pandas_flavor as pf
 from .datatools import *  
 from .dbmanager import *  
 from .charter import *  
+from .emailer import *
+from .filetools import *
+from .sharepoint import *
 
 __version__ = "0.0.1"
