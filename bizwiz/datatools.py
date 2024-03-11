7dd1f5d3f2398f7921735e165d51e2a30cf092c2
Solomon Moon <57546826+soloemoon@users.noreply.github.com>
2024-03-10 23:47:38 -0500

    Update datatools.py
    Added documentation strings to functions

--- bizwiz/datatools.py
+++ bizwiz/datatools.py
@@ -2,10 +2,11 @@ from typing import Dict, Union
 
 import pandas_flavor as pf
 import pandas as pd
+import numpy as np
 from datetime import datetime, timedelta
 
 def _date_range_calc(start_date, end_date, number_of_days, date_format='%Y-%m-%d'):
-
+# Helper function to calculate date range
     start_date = datetime.striptime(start_date, date_format)
     end_date = datetime.striptime(end_date, date_format)
     
@@ -18,13 +19,13 @@ def _date_range_calc(start_date, end_date, number_of_days, date_format='%Y-%m-%d
 
     return dl    
 
-def create_date_list(
-          start_date: str, 
-          end_date: str, 
-          number_of_days: int=1, 
-          date_format: str='%Y-%m-%d', 
-          remove_weekends: bool=False
-        ) ->list:
+def create_date_list( 
+    start_date: str, 
+    end_date: str, 
+    number_of_days: int=1, 
+    date_format: str='%Y-%m-%d', 
+    remove_weekends: bool=False
+) -> list:
     
     """Creates a list of dates between a specified start and end date based on a defined interval.
 
@@ -66,10 +67,10 @@ def create_date_list(
     return date_list
     
 def create_month_list(
-          start_date: str, 
-          end_date: str, 
-          date_format: str='%Y-%m-%d'
-          ) ->list:
+    start_date: str, 
+    end_date: str, 
+    date_format: str='%Y-%m-%d'
+) -> list:
     
     """Creates a list of months between a specified start and end date.
 
@@ -107,11 +108,11 @@ def _business_days(start_date, end_date):
     WEEKDAY_FRIDAY = 4 
     
     if start_date.weekday() > WEEKDAY_FRIDAY:
-        start_date = start_date + timedelta(days=7 - start_date.weekday())
+        start_date = start_date + timedelta( days=7 - start_date.weekday() )
 
     # if the end date is on a weekend, rewind the date to the previous Friday
     if end_date.weekday() > WEEKDAY_FRIDAY:
-        end_date = end_date - timedelta(days=end_date.weekday() - WEEKDAY_FRIDAY)
+        end_date = end_date - timedelta( days=end_date.weekday() - WEEKDAY_FRIDAY )
 
     if start_date > end_date:
         return 0
@@ -133,9 +134,9 @@ def date_diff(
     end_date_column: str, 
     date_format: str = '%Y-%m-%d', 
     calculation: str='days'
-    ) ->pd.DataFrame:
+) -> pd.DataFrame:
         
-        """Computes the difference between two dataframe date columns in calendar/business days, months, or years.
+    """Computes the difference between two dataframe date columns in calendar/business days, months, or years.
 
         If date columns are formatted as strings then they are converted to datetime objects.
 
@@ -162,77 +163,219 @@ def date_diff(
 
         Returns:
             DataFrame that has date differenced columns.
-        """
-        start_list = [pd.to_datetime(x) for x in df[start_date_column].tolist()]
-        end_list = [pd.to_datetime(x) for x in df[end_date_column].tolist()]
+    """
+    start_list = [pd.to_datetime(x) for x in df[start_date_column].tolist()]
+    end_list = [pd.to_datetime(x) for x in df[end_date_column].tolist()]
 
-        calculation = calculation.lower()
+    calculation = calculation.lower()
 
-        if calculation in['day', 'd', 'days']:
-            calendar_diff_list = [(x - y).days for x, y in zip(end_list, start_list)]
-            business_day_diff_list = [_business_days(x, y) for x,y in zip(start_list, end_list)]
+    if calculation in['day', 'd', 'days']:
+        calendar_diff_list = [(x - y).days for x, y in zip(end_list, start_list)]
+        business_day_diff_list = [_business_days(x, y) for x,y in zip(start_list, end_list)]
 
-            df['date_diff_calendar_days'] = calendar_diff_list
-            df['date_diff_business_days'] = business_day_diff_list
+        df['date_diff_calendar_days'] = calendar_diff_list
+        df['date_diff_business_days'] = business_day_diff_list
         
-        elif calculation in['month', 'months']:
-            month_diff_list = [(x.year - y.year) * 12 + x.month - y.month for x, y in zip(end_list, start_list)]
+    elif calculation in['month', 'months']:
+        month_diff_list = [(x.year - y.year) * 12 + x.month - y.month for x, y in zip(end_list, start_list)]
 
-            df['date_diff_months']  = month_diff_list
+        df['date_diff_months']  = month_diff_list
         
-        elif calculation in['year', 'years']:
-            year_diff_list = [(x.year - y.year) for x, y in zip(end_list, start_list)]
+    elif calculation in['year', 'years']:
+        year_diff_list = [(x.year - y.year) for x, y in zip(end_list, start_list)]
 
-            df['date_diff_years']  = year_diff_list
+        df['date_diff_years']  = year_diff_list
              
-        return df
+    return df
 
 
 @pf.register_dataframe_method
-def dict_from_df(df, key_col, value_col):
+def dict_from_df(
+    df: pd.DataFrame, 
+    key_column: str, 
+    value_column: str
+) -> dict:
+
+    """Create a dictionary from dataframe columns
+
+            Examples:
+                Functional usage
+
+                >>> import pandas as pd
+                >>> from bizwiz import datatools as dt
+                >>> df = df.dict_from_df(
+                ... key_column = 'A'
+                ... value_column = 'B'
+                ... )  # doctest: +SKIP
+
+
+            Args:
+                df: Primary dataframe
+                key_column: Column name containing the values to be used dictionary keys
+                value_column: Column name containing the values to be used as dictionary values
+                
+            Returns:
+                Dictionary with key, value combinations based on dataframe columns
+            """
         
-    t_dict = df.set_index(key_col).to_dict()[value_col]
+    t_dict = df.set_index(key_column).to_dict()[value_column]
 
     return t_dict
 
 @pf.register_dataframe_method
-def retain_leading_zero(df, column_names):
+def retain_leading_zero(
+    df: pd.DataFrame, 
+    column_names:list
+):
 
-    if isinstance(column_names, list):
-        for col in column_names:
-            df[col] = '="' + df[col] +'"'
-    
-    if isinstance(column_names, str):
-        df[col] = '="' + df[col] +'"'
-    
+    if isinstance(column_names) == str:
+        column_names = list(column_names)
+
+    for col in column_names:
+        df[col] = df[col].apply('="{}"'.format)
+
     return df
 
 @pf.register_dataframe_method
-def fill_leading_zero(df, column_names, character_length=10):
+def fill_leading_zero(
+    df: pd.DataFrame, 
+    column_names: str, 
+    character_length: int=10
+) -> pd.DataFrame:
 
     df[column_names] = df[column_names].astype(str).str.zfill(character_length)
     
     return df
 
+@pf.register_dataframe_method
+def subset_by_group(
+    df: pd.DataFrame, 
+    column_name: str
+) -> dict:
+
+    return dict(iter(df.groupby(column_name)))
+
+@pf.register_dataframe_method
+def dataframe_compare(
+    df: pd.DataFrame, 
+    df2: pd.DataFrame,
+) -> pd.DataFrame:
+        
+    """Compare two dataframes and output a dataframe of values that changed.
+
+            Examples:
+                Functional usage
+
+                >>> import pandas as pd
+                >>> from bizwiz import datatools as dt
+                >>> df = df.dataframe_compare(df2
+                ... )  # doctest: +SKIP
 
 
-        
-        
+            Args:
+                df: Primary dataframe
+                df2: dataframe to be compared to the primary
+                
+
+            Returns:
+                DataFrame that has breakdown of the values that changed
+    """
     
+    ne_stacked = (df != df2).stack()
+    changed = ne_stacked[ne_stacked]
+    changed.index.names = ['id', 'col']
+    difference_locations = np.where(df != df2)
+    changed_from = df.values[difference_locations]
+    changed_to = df2.values[difference_locations]
+    result = pd.DataFrame({'from': changed_from, 'to': changed_to}, index=changed.index)
+    return result
+
+@pf.register_dataframe_method
+def unique_records_between_df(
+    df: pd.DataFrame, 
+    df2: pd.DataFrame, 
+    column_names: list
+) -> pd.DataFrame:
+    """Get the unique records between two like dataframe columns
+
+            Examples:
+                Functional usage
+
+                >>> import pandas as pd
+                >>> from bizwiz import datatools as dt
+                >>> df = df['lookup_column'].create_flag(
+                ...     flag_name = 'mapped_values',
+                ...     lookup_list = ['A', 'B', 'C']
+                ...     map_if_in_list = True
+                ... )  # doctest: +SKIP
+
+
+            Args:
+                df: DataFrame containing the data to map
+                df2: 
+                column_names:
+                
+
+            Returns:
+                DataFrame that has date differenced columns.
+    """
     
+    if isinstance(column_names) == str:
+        column_names = list(column_names)
+
+    result = pd.merge(df, df2, on=column_names, how = 'outer', indicator=True)
+    result = result[result._merge != 'both'] # Filter out records from both
+
+    return result
+
+@pf.register_dataframe_method
+def create_flag(
+        df: pd.DataFrame, 
+        lookup_column: str, 
+        flag_name: str, 
+        lookup_list: list, 
+        overwrite_values: bool = True, 
+        default_value: str = 'N', 
+        flag_value: str = 'Y',
+        map_if_in_list: bool = True
+) -> pd.DataFrame:
+
+    """Creates a flag column in dataframe based on the values in a list. Values can be mapped based on either being present in the list or not being present in the list.
+
+        Examples:
+            Functional usage
+
+            >>> import pandas as pd
+            >>> from bizwiz import datatools as dt
+            >>> df = df['lookup_column'].create_flag(
+            ...     flag_name = 'mapped_values',
+            ...     lookup_list = ['A', 'B', 'C']
+            ...     map_if_in_list = True
+            ... )  # doctest: +SKIP
+
+
+        Args:
+            df: DataFrame containing the data to map
+            lookup_column: Column name containing the values to compare against the list
+            flag_name: Desired output column name of the mapped flag
+            lookup_list: List of values to be compared against dataframe for mapping
+            overwrite_values: Boolean that determines if the default value (N) will be used as flag or if the original column values will be maintained. Default is True.
+            default_value: The default value if the flag is missing. Currently 'N'
+            flag_value: The value to return if a mapping is possible. Default is 'Y'
+            map_if_in_list: Set to true to map the values if they exist in the inputted list. Select false to map the values if they are not in the list.
+            
+
+        Returns:
+            DataFrame that has a generated flag column.
+        """
+
+    if overwrite_values == True:
+        df[flag_name] = default_value
     
+    if map_if_in_list  == True:
+        df.loc[(df[lookup_column].isin(lookup_list)), flag_name] = flag_value
     
+    elif map_if_in_list == False:
+        df.loc[~(df[lookup_column].isin(lookup_list)), flag_name] = flag_value
     
-        
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    return df
